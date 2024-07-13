7df19672b890f5a44d43a476fe502cfc0cf43f36
pratibhaWebDev <pratibhacodedev@gmail.com>
2024-07-13 13:25:12 +0530

    Day 1 challenge

---
+++ Day1.js
@@ -0,0 +1,59 @@
+// Day 1
+// Activity 1: Variable Declaration 
+// Task 1
+var num=2001
+console.log("var num value =>",num)
+
+// Task 2
+let name="Pratibha"
+console.log("let name value =>",name)
+
+// Activity 2: Constant Declaration 
+// Task 3
+const valueBool=0
+console.log("boolen value ",valueBool);
+
+// Activity 3: Data Types 
+// Task 4
+// (number,string,boolean,object,array)
+let num1=811;
+console.log("Data Type Num",num1)
+console.log("typeof number =>",typeof num1)
+
+
+let string="Pratibha Yadav"
+console.log("Data Type String",string)
+console.log("typeof String =>",typeof string)
+
+
+let bool=1
+console.log("Data Type Boolean",bool)
+console.log("typeof Boolean =>",typeof bool)
+
+
+let obj={
+    name:"Pratibha",
+    age:22,
+}
+console.log("Data Type Object",obj)
+console.log("typeof Object =>",typeof obj)
+
+let Array=[2,3,5,6]
+console.log("Data Type Array",Array)
+console.log("typeof Array =>",typeof Array)
+
+// Activity 4: Reassigning Varible 
+// Task 5:
+let val1=99;
+console.log("val1 value =>",val1)
+val1=100;
+console.log("val1 value =>",val1)
+
+// Activiy 5: Understanding const
+// Task 6:
+const val2=100;
+console.log("val2 value =>",val2)
+val2=200;
+console.log("val2 value =>",val2)
+
+//const immutability 
