cf1f96db9d84b011336201c7cc3d176eded7ee38
pratibhaWebDev <pratibhacodedev@gmail.com>
2024-07-14 12:29:17 +0530

    Add DAy2.js

---
+++ Day2.js
@@ -0,0 +1,53 @@
+// Day 2: Operators
+// Activity 1: Arithmetic Operation
+// Task 1
+var a=5,b=5
+var sum=a+b;
+console.log("Sum of Two Num",sum);
+//Task 2
+var sub=a-b;
+console.log("Sub of Two Num",sub);
+//Task 3
+var Multiply=a*b;
+console.log("Multiply of Two num",Multiply)
+//task 4
+var divide=a/b;
+console.log("Divide of Two num",divide)
+//task 5
+var rem=a%b;
+console.log("Remainder of Two num",rem);
+
+// Activity 2: Assignment Operators
+// Task 6
+var c=0
+c+=3;
+console.log("Sum of Two Num",c);
+//Task 7
+var d=0
+d-=3;
+console.log("Sub of Two Num",d);
+
+// Activity 3: Comparison Operators
+// Task 8
+var comp1 =8 , comp2=7;
+console.log("Greater than",comp1>comp2) 
+console.log("Less than",comp1<comp2)
+console.log("Greater than or equal to",comp1>=comp2)
+console.log("Less than or equal to",comp1<=comp2)
+console.log("Equal to",comp1==comp2)
+console.log("Equal to triple",comp1===comp2)
+console.log("Not equal to",comp1!=comp2)
+
+// Activity 4: Logical Operators
+// Task 11
+var oper1=10,oper=9
+console.log("And",oper1&&oper)
+console.log("Or",oper1||oper)
+console.log("Not",!oper1)
+
+//Activity 5: Ternary Operator
+// Task 14
+var num1=10
+var result=num1>0? "Positive" : "Negtive";
+console.log("Ternary Operator",result)
+
